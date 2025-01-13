866e389167808a6f63c46576e2dfb2a1dde0850a
Rabbilkhan <rabbilkhan125@gmail.com>
2025-01-14 03:53:30 +0530

    url issue fixed

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
-
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import cors from 'cors'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import listingRouter from './routes/listing.route.js'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
 const PORT = 5000;
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: "https://homestate25.netlify.app", // Replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // If you’re using cookies or authentication headers
+  })
+);
 
-app.use(cors({
-    origin: 'https://homestate25.netlify.app', // Replace with your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // If you’re using cookies or authentication headers
-}));
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-mongoose.connect(process.env.MONGO).then(()=>{
-console.log('Connected to MongoDB!')
-
-}) 
-.catch((err)=>{
-    console.log(err)
-})
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-
-
-app.get('/', (req,res)=>{
-    res.send('App working fine')
-})
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter)
-app.use('/api/listing', listingRouter)
-
-app.use((err,req,res,next)=>{
-
-const statusCode = err.statuscode || 500;
-
-const message = err.message || 'Internal Server Error'
-return res.status(statusCode).json({
-    success:false,
-    statusCode,
-    message
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
+app.get("/", (req, res) => {
+  res.send("App working fine");
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statuscode || 500;
+
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
